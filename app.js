e2b5a98a6a4a9cd6c8577e4d6517fe7dcce9e9fc
Siddharth Krish <siddharth.krish@moneysmart.sg>
2018-04-17 23:27:40 +0800

    added moment js

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.locals.moment = require('moment');
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
